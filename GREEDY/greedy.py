2fad791373eb1349648894bfa8d3d5dc712ecc64
PaperWings2019 <56267379+PaperWings2019@users.noreply.github.com>
2023-01-04 15:34:46 +0000

    Update greedy.py

--- GREEDY/greedy.py
+++ GREEDY/greedy.py
@@ -61,3 +61,16 @@ def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         queue.insert(people[i][1], people[i])
         # print(queue)
     return queue
+
+
+def findMinArrowShots(self, points: List[List[int]]) -> int:
+    n = len(points)
+    if n == 0: return 0
+    result = 1
+    points.sort(key = lambda x: x[0])
+    for i in range(1, n):
+        if points[i][0] > points[i - 1][1]:
+            result += 1
+        else:
+            points[i][1] = min(points[i][1], points[i - 1][1])
+    return result
