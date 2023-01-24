caa8cdd0212929e6c39b637a19b8b8e3dcf11284
PaperWings2019 <56267379+PaperWings2019@users.noreply.github.com>
2023-01-24 10:20:04 +0000

    Update dp.py

--- DynamicPlanning/dp.py
+++ DynamicPlanning/dp.py
@@ -65,3 +65,17 @@ def lastStoneWeightII(self, stones: List[int]) -> int:
         for j in range(target, stones[i] - 1, -1):
             dp[j] = max(dp[j], dp[j - stones[i]] + stones[i])
     return totalWeight - dp[target] - dp[target]
+
+
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    if (sum(nums) + target) % 2 == 1:
+        return 0
+    if abs(target) > sum(nums):
+        return 0
+    left = int((sum(nums) + target) / 2)
+    dp = [0] * (left + 1)
+    dp[0] = 1
+    for i in range(len(nums)):
+        for j in range(left, nums[i] - 1, -1):
+            dp[j] += dp[j - nums[i]]
+    return dp[left]
